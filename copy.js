d10a0c358df6993a036b2cfc44c1407cac1c263b
lonelyfavourchisale <favourgchisale@gmail.com>
2023-01-29 15:07:22 -0800

    port

--- copy.js
+++ copy.js
@@ -8,7 +8,7 @@ var accountSid = 'AC9409d8e6d57b658546a1a78de152b163';
 var authToken  = '05aa90e3d44834c309de115e64a18f9d';
 const client = require('twilio')(accountSid, authToken);
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3030
 
 app.use(logger('dev'))
 app.use(bodyParser.json())
