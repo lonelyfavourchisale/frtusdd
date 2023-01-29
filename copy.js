174895fe4aee8c71ec19aadd80ca1fa4757d61a9
lonelyfavourchisale <favourgchisale@gmail.com>
2023-01-29 15:56:42 -0800

    addet

--- copy.js
+++ copy.js
@@ -135,7 +135,10 @@ const firebaseConfig = {
     })
 
   })
- 	
+ 
+  app.get('*', (req, res) => {
+    console.log('hello')
+  })	
 	app.post('*', (req, res) => {
 	let {sessionId, serviceCode, phoneNumber, text} = req.body
 	
