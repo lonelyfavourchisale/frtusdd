b5d35bed0d4f0bd399f18c389d686d8df3f728ae
lonelyfavourchisale <favourgchisale@gmail.com>
2023-01-29 23:39:21 -0800

    frtussdstart

--- index.js
+++ index.js
@@ -9,43 +9,24 @@ const app = require('express')()
 	app.use(bodyParser.urlencoded({extended: true}))
 	
 	app.get('*', (req, res) => {
-	res.send('This is tutorial App on creating your first USSD app in 5 minutes or less by Ajala Abdulsamii <kgasta@gmail.com>')
+	res.send('USSD deployed successfully')
 	})
 	
 	app.post('*', (req, res) => {
-	let {sessionId, serviceCode, phoneNumber, text} = req.body
-	if (text == '') {
-	// This is the first request. Note how we start the response with CON
-	let response = `CON What would you want to check
-	1. My Account
-	2. My phone number`
-	res.send(response)
-	} else if (text == '1') {
-	// Business logic for first level response
-	let response = `CON Choose account information you want to view
-	1. Account number
-	2. Account balance`
-	res.send(response)
-	} else if (text == '2') {
-	// Business logic for first level response
-	let response = `END Your phone number is ${phoneNumber}`
-	res.send(response)
-	} else if (text == '1*1') {
-	// Business logic for first level response
-	let accountNumber = 'ACC1001'
-	// This is a terminal request. Note how we start the response with END
-	let response = `END Your account number is ${accountNumber}`
-	res.send(response)
-	} else if (text == '1*2') {
-	// This is a second level response where the user selected 1 in the first instance
-	let balance = 'NGN 10,000'
-	// This is a terminal request. Note how we start the response with END
-	let response = `END Your balance is ${balance}`
-	res.send(response)
-	} else {
-	res.status(400).send('Bad request!')
-	}
-	})
+	let {sessionId, serviceCode, phoneNumber, text,response} = req.body
+	 //first 
+	 if(text == '') {
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~First request for the FRT main menu~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        response = `CON Welcome to Farm Radio Trust
+        1. Register
+        2. Main Menu
+        3. Help`;
+    }
+ //send the response back
+ res.set('Content-Type: text/plain');
+ res.send(response)
+})
 	
 	app.listen(port, () => {
 	console.log(`Server running on port ${port}`)
