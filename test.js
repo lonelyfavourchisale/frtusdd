1611bae7063ca9d3dd1e35026a4b3e10fdeac479
lonelyfavourchisale <favourgchisale@gmail.com>
2023-02-05 02:45:47 -0800

    adding next line

--- test.js
+++ test.js
@@ -8,3 +8,20 @@ names.forEach(element => {
 }
 namess()
 console.log(emptyarray)
+
+snapshot.forEach((childSnapshot) => {
+    datadistrict = ``;
+    var datadistrict = `${childSnapshot.val().name}`;
+    districts.push(datadistrict);
+  });
+
+  const indexDistricts = districts.map(
+    (ds, index) => `${index + 1}. ${ds}`
+  );
+
+  var array=indexDistricts.toString().split(',')
+  console.log(array.join('\n'))
+
+  response = `CON choose district for weather \n ${indexDistricts.toString()}`;
+});
+}
