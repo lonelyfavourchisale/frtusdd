185f575f0a6ff52c8e27a873488df56eeabd7002
lonelyfavourchisale <favourgchisale@gmail.com>
2023-02-05 13:13:44 -0800

    index from input

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ response=`CON choose weather information category \n
 
    else if(dataarray[1]=='2' && dataarraysize==4 && dataarray[3]==1){
     function getactions(){
-      var index='/8';
+      var index=`${dataarray[2]}`;
       const actionsref=ref.child(index)
     
       
@@ -134,7 +134,7 @@ response=`CON choose weather information category \n
 
   else if(dataarray[1]=='2' && dataarraysize==4 && dataarray[3]==2){
     function getexpected(){
-      var index='/8';
+      var index=`${dataarray[2]}`;
       const actionsref=ref.child(index)
     
       
@@ -164,7 +164,7 @@ response=`CON choose weather information category \n
  else if(dataarray[1]=='2' && dataarraysize==4 && dataarray[3]==3){
     
 function weeklyweather(){
-  var index='/8';
+  var index=`${dataarray[2]}`;
   var tempindex='/1'
   const actionsref=ref.child(index + '/weeklyTemps' + tempindex)
 
@@ -177,7 +177,7 @@ function weeklyweather(){
       var mintemp=snapshot.val().min
       var status=snapshot.val().status
 
-    response=`CON weekly weather report for ${title} \n 
+    response=`END weekly weather report for ${title} \n 
     maximum temperature is : ${maxtemp} \n
     minimum temperature is : ${mintemp}
     weather status is : ${status}`
