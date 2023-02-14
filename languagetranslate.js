ddb9517669c1af12d0c152616e68ecc9c790b4cf
lonelyfavourchisale <favourgchisale@gmail.com>
2023-02-13 23:14:52 -0800

    languagetranslation

--- languagetranslate.js
+++ languagetranslate.js
@@ -1,7 +1,9 @@
 const translator = require('translate');
+const response = 'hello there'
+
 async function languagetranslator(message,translateto){
     translateto.engine ='libre'
     const translatedstring = await translator(message,translateto)
     console.log(translatedstring)
 }
-languagetranslator('programming is fun','chichewa')
+languagetranslator(response,'chichewa')
